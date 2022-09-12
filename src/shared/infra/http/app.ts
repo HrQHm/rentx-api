a449c8c35bd6fda911d2c7b99703baa73c0c2c63
HrQHm <henrique_hmk@hotmail.com>
2022-09-12 11:35:58 -0300

    Cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -4,6 +4,7 @@ import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
 import swaggerFile from '../../../swagger.json';
+import cors from "cors";
 
 import '@shared/container';
 import { AppError } from "@shared/errors/AppError";
@@ -17,6 +18,7 @@ app.use(express.json());
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use(cors());
 app.use(router);
 
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
